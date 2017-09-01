94b77ddc5d992c092ab6d9bf8d09b4a927a3632b
Andrew Greenough <andygreenough@gmail.com>
2017-09-01 18:20:25 +0100

    Building CreateGoalVC

---
+++ goalpost/Controller/CreateGoalVC.swift
@@ -0,0 +1,37 @@
+//
+//  CreateGoalVC.swift
+//  goalpost
+//
+//  Created by Andrew Greenough on 01/09/2017.
+//  Copyright © 2017 Andrew Greenough. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalVC: UIViewController {
+    
+    // Outlets
+    @IBOutlet weak var goalTextView: UITextView!
+    @IBOutlet weak var shortTermBtn: UIButton!
+    @IBOutlet weak var longTermBtn: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func nextBtnWasPresssed(_ sender: Any) {
+    }
+    
+    @IBAction func shortTermBtnWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func longTermBtnWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func backBtnWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
